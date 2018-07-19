bc66cd3551ea93d51006b53e5f3a54c6366a4985
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-19 21:53:01 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,7 +4,7 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    collection << yield(array[i])
+    collection << yield(array[i]) #=> captures the return values of yielding those items to the block
     i += 1
   end
   collection
